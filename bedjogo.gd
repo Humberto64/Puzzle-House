7dcf4bd2f447e262a8bb1b17f42a7ace094a2bf9
GustavoIGD <a2024110312@estudiantes.upsa.edu.bo>
2025-05-28 10:28:18 -0400

    Add files via upload

---
+++ bedjogo.gd
@@ -0,0 +1,95 @@
+extends Area2D
+
+var tiles = []
+var solved = []
+var mouse = false
+
+func _ready():
+	# Referencia a las fichas
+	tiles = [$Tile1, $Tile2, $Tile3, $Tile4, $Tile5, $Tile6, $Tile7, $Tile8,
+			 $Tile9, $Tile10, $Tile11, $Tile12, $Tile13, $Tile14, $Tile15, $Tile16 ]
+	solved = tiles.duplicate()
+	shuffle_tiles()
+
+	# Oculta la pantalla de victoria al inicio
+	$CanvasLayer/VictoryScreen.visible = false
+
+	# Conecta el botón de salida
+	$CanvasLayer/VictoryScreen/ExitButton.pressed.connect(_on_exit_button_pressed)
+
+func start_game():
+	shuffle_tiles()
+
+func shuffle_tiles():
+	var previous = 99
+	var previous_1 = 98
+	for t in range(0,1000):
+		var tile = randi() % 16
+		if tiles[tile] != $Tile16 and tile != previous and tile != previous_1:
+			var rows = int(tiles[tile].position.y / 250)
+			var cols = int(tiles[tile].position.x / 250)
+			check_neighbours(rows,cols)
+			previous_1 = previous
+			previous = tile
+
+func _process(delta):
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT) and mouse:
+		var mouse_copy = mouse
+		mouse = false
+		var rows = int(mouse_copy.position.y / 250)
+		var cols = int(mouse_copy.position.x / 250)
+		check_neighbours(rows,cols)
+		if tiles == solved:
+			show_victory_screen()
+
+func check_neighbours(rows, cols):
+	var empty = false
+	var done = false
+	var pos = rows * 4 + cols
+	while !empty and !done:
+		var new_pos = tiles[pos].position
+		if rows < 3:
+			new_pos.y += 250
+			empty = find_empty(new_pos,pos)
+			new_pos.y -= 250
+		if rows > 0:
+			new_pos.y -= 250
+			empty = find_empty(new_pos,pos)
+			new_pos.y += 250
+		if cols < 3:
+			new_pos.x += 250
+			empty = find_empty(new_pos,pos)
+			new_pos.x -= 250
+		if cols > 0:
+			new_pos.x -= 250
+			empty = find_empty(new_pos,pos)
+			new_pos.x += 250
+		done = true
+
+func find_empty(position,pos):
+	var new_rows = int(position.y / 250)
+	var new_cols = int(position.x / 250)
+	var new_pos = new_rows * 4 + new_cols
+	if tiles[new_pos] == $Tile16:
+		swap_tiles(pos, new_pos)
+		return true
+	else:
+		return false
+
+func swap_tiles(tile_src, tile_dst):
+	var temp_pos = tiles[tile_src].position
+	tiles[tile_src].position = tiles[tile_dst].position
+	tiles[tile_dst].position = temp_pos
+	var temp_tile = tiles[tile_src]
+	tiles[tile_src] = tiles[tile_dst]
+	tiles[tile_dst] = temp_tile
+
+func _input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton:
+		mouse = event
+
+func show_victory_screen():
+	$CanvasLayer/VictoryScreen.visible = true
+
+func _on_exit_button_pressed():
+	get_tree().quit()
